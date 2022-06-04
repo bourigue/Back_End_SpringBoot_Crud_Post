cf84667af0838ca83dd0eca8b0cecca6e74f3787
bourigue <rrbourigue@gmail.com>
2022-06-04 16:03:48 +0100

    fix:fixed the entite Post

---
+++ src/main/java/com/example/post/Entites/Post.java
@@ -0,0 +1,20 @@
+package com.example.post.Entites;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+@Entity
+@AllArgsConstructor @NoArgsConstructor @ToString
+public class Post implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String userId;
+    private String description;
+}

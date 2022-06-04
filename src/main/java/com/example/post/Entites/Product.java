deb75f0d2a28fbd5a6d0bec6e4e9271e2d3a1d88
bourigue <rrbourigue@gmail.com>
2022-06-04 15:56:20 +0100

    fix:fixed the entite Post

--- src/main/java/com/example/post/Entites/Product.java
+++ src/main/java/com/example/post/Entites/Product.java
@@ -1,2 +1,26 @@
-package com.example.post.Entites;public class Product {
+package com.example.post.Entites;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+
+@Data
+@Entity
+@NoArgsConstructor @AllArgsConstructor @ToString
+public class Product implements Serializable {
+    @Id @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private  Long Id ;
+    private String name;
+    private double price ;
+    private String desctiption;
+    private  int quantity;
+
 }

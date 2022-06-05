9edbb9e30eb952f5c91197a0c9a936b516033327
bourigue <rrbourigue@gmail.com>
2022-06-05 16:55:42 +0100

    fix:finixed rest aoi

--- src/main/java/com/example/post/Repositories/PostRepository.java
+++ src/main/java/com/example/post/Repositories/PostRepository.java
@@ -4,9 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.List;
-
+@CrossOrigin(origins ="http://localhost:4200")
 @RepositoryRestResource
 public interface PostRepository extends JpaRepository<Post, Long> {
 

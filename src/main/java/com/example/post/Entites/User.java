3ae40620d18ecfa7445d3ebff3569d0dfff66357
bourigue <rrbourigue@gmail.com>
2022-06-06 21:55:36 +0100

    fix:add  Entities User

---
+++ src/main/java/com/example/post/Entites/User.java
@@ -0,0 +1,27 @@
+package com.example.post.Entites;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Data
+public class User  implements Serializable {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+    private int age;
+
+}

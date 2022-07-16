6c04739397a134a02cca2cc34a2777d56a842e7e
bourigue <rrbourigue@gmail.com>
2022-07-16 11:21:34 +0100

    feat:add Junit testing

---
+++ src/test/java/com/example/post/Services/PostServiceTest.java
@@ -0,0 +1,26 @@
+package com.example.post.Services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PostServiceTest {
+
+    @Test
+    void getPosts() {
+
+    }
+
+    @Test
+    void getPost() {
+    }
+
+    @Test
+    void savePost() {
+
+    }
+
+    @Test
+    void deletePost() {
+    }
+}
